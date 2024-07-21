4a057e146896a47455ab1022b56056e9f2fcec80
yash roushan <yashroushan@gmail.com>
2024-07-21 13:26:52 -0300

    assignment 3 on personal github

---
+++ prosim/cmake-build-debug/CMakeFiles/prosim.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prosim.dir/context.c.o"
+  "CMakeFiles/prosim.dir/main.c.o"
+  "CMakeFiles/prosim.dir/prio_q.c.o"
+  "CMakeFiles/prosim.dir/process.c.o"
+  "prosim"
+  "prosim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/prosim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
